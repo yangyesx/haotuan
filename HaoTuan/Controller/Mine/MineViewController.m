692526ed97ff6deab919eede5c84c8219750d81f
杨允恩 <yangye_sx@taofen8.com>
2016-10-04 10:22:48 +0800

    mine header

--- HaoTuan/Controller/Mine/MineViewController.m
+++ HaoTuan/Controller/Mine/MineViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "MineViewController.h"
+#import <Masonry.h>
 
-@interface MineViewController ()
+@interface MineViewController ()<UIScrollViewDelegate>
 
 @property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, strong) UIImageView *photoImgV;
+@property (nonatomic, strong) UILabel *nickLabel;
 
 @end
 
@@ -23,24 +26,34 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     self.scrollView = [[UIScrollView alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 1.3);
+    self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height * 1.1);
+    self.scrollView.delegate = self;
     
     [self.view addSubview:self.scrollView];
+    
+    self.photoImgV = [UIImageView new];
+    self.photoImgV.backgroundColor = [UIColor redColor];
+    self.photoImgV.layer.cornerRadius = 40;
+    [self.scrollView addSubview:self.photoImgV];
+    [self.photoImgV mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(80, 80));
+        make.left.mas_equalTo(40);
+        make.top.mas_equalTo(60);
+    }];
+    
+    self.nickLabel = [UILabel new];
+    self.nickLabel.text = @"杨允恩";
+    [self.scrollView addSubview:self.nickLabel];
+    [self.nickLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.photoImgV.mas_right).multipliedBy(1.2);
+        make.centerY.mas_equalTo(self.photoImgV);
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
